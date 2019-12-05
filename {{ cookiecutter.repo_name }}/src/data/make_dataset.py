c86d922301d00dd956302efc00638ab0b3feba82
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2019-12-05 12:01:07 +0100

    No arguments for make_dataset

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -3,16 +3,17 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
-def main(input_filepath, output_filepath):
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
